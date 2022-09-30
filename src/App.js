54a2051df550846d9b3c28778ac9ae684fe5240a
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-30 08:57:56 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
-
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import FilmList from './FilmList'
 import Details from './Details'
-import TMDB from './TMDB'
 
 export default function App() {
-  const [films, setFilms] = useState(TMDB.films)
+  const [films, setFilms] = useState([])
   const [current, setCurrent] = useState({})
+  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=ca0f95deafc420a0f20e60ceb7a940c4&language=en-US&page=1`;
+
+  useEffect(() => {
+    // const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    fetch(popularFilmsUrl)
+      .then(response => response.json())
+      .then(jsonDeets => {
+        setFilms(jsonDeets.results)
+        console.log(films)
+      }, [])
+  }, [])
 
   const handleDetailsClick = film => {
     console.log('fetching details for film:', film.title)
     setCurrent(film)
-}
+  }
 
 
   return (
     <div className="film-library">
-      <FilmList 
-        films={films} 
+      <FilmList
+        films={films}
         handleDetailsClick={handleDetailsClick}
       />
 
-      <Details 
-        films={TMDB.films} 
+      <Details
+        films={films}
         film={current}
       />
     </div>
