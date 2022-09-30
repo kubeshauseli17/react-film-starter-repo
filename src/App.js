c0eb5b40f44522a59088bf0a53feb745483236cc
kubeshauseli17 <kubeshauseli17@gmail.com>
2022-09-30 09:16:29 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import FilmList from './FilmList'
+import TMDB from './TMDB'
 import Details from './Details'
 
 export default function App() {
   const [films, setFilms] = useState([])
   const [current, setCurrent] = useState({})
-  const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=ca0f95deafc420a0f20e60ceb7a940c4&language=en-US&page=1`;
 
   useEffect(() => {
-    // const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
+    const popularFilmsUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${TMDB.api_key}&language=en-US&page=1`;
     fetch(popularFilmsUrl)
       .then(response => response.json())
       .then(jsonDeets => {
